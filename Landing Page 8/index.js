52d7be233f11786d7c6de7419f71a2854f06cf07
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-08-17 17:45:50 +0530

    Landing page 8 commit

---
+++ Landing Page 8/index.js
@@ -0,0 +1,6 @@
+gsap.from(".nav", { y: -100, delay: 0.5, opacity: 0, duration: 1 });
+gsap.from(".image_model", { delay: 2.8, opacity: 0, duration: 2 });
+gsap.from(".scroll-div", { y: 100, delay: 1.2, opacity: 0, duration: 2 });
+gsap.from(".scroll-div2", { y: 100, delay: 1.7, opacity: 0, duration: 2 });
+gsap.from(".scroll-div3", { y: 100, delay: 2.3, opacity: 0, duration: 2 });
+gsap.from(".text", { delay: 2.8, opacity: 0 });
