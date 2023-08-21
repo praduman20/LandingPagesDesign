52a5b5f1010d1c0f7909083055072fff9c8daaae
Praduman Singh Chundawat <pschundawat4080@gmail.com>
2023-08-21 17:40:47 +0530

    Landing page 7 commit

---
+++ Landing Page 7/index.js
@@ -0,0 +1,5 @@
+gsap.from(".top", { opacity: 0, y: -200, delay: 0.5, duration: 1 });
+gsap.from(".bone", { opacity: 0, y: 200, delay: 0.8, duration: 1 });
+gsap.from(".btwo", { opacity: 0, y: 200, delay: 1.1, duration: 1 });
+gsap.from(".bthree", { opacity: 0, y: 200, delay: 1.4, duration: 1 });
+gsap.from(".bfour", { opacity: 0, y: 200, delay: 1.7, duration: 1 });
